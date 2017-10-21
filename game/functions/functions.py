25f69b77bb062fdea81896953ea9a6bf44433cb9
Ahish <ahishjayant9009@gmail.com>
2017-10-21 18:02:02 +0400

    Create functions.py

---
+++ game/functions/functions.py
@@ -0,0 +1,9 @@
+import pygame as pg
+
+#to load images
+def load_images():
+    bg=pg.image.load('pictures/bg.png')
+    rod=pg.image.load('pictures/rod.png')
+    playerr=pg.image.load('pictures/player red.jpg')
+    playerb=pg.image.load('pictures/player blue.jpg')
+    volleyball=pg.image.load('pictures/volleyball.png')
