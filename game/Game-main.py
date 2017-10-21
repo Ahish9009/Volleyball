22b82240be10979d5de6d237c0e13d737fac337b
Ahish <ahishjayant9009@gmail.com>
2017-10-21 17:58:54 +0400

    Update Game-main.py

--- game/Game-main.py
+++ game/Game-main.py
@@ -1,2 +1,10 @@
 #Volleyball simulation
-import pygame as pg, math, random
+import pygame as pg, math
+
+#screen size
+screen_size=(800,600)
+
+#initializes object for framerate
+clock=pg.time.Clock()
+
+
