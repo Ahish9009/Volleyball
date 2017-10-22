0edfacfb9c0ca4c21e6c3e24c0e5dbf5337effce
Ahish <ahishjayant9009@gmail.com>
2017-10-22 21:54:23 +0400

    Update Game-main.py

--- game/Game-main.py
+++ game/Game-main.py
@@ -1,3 +1,4 @@
+
 #Volleyball simulation
 import pygame as pg, math
 import functions as fn
@@ -8,8 +9,7 @@ pg.init()
 screen_size=(800,600)
 
 #loads and transforms images
-fn.load_images()
-fn.transform_images()
+bg,rod,playerr,playerb,volleyball=fn.load_images()
 
 #initializes object for framerate
 clock=pg.time.Clock()
@@ -20,7 +20,7 @@ pg.display.set_caption('Volleyball')
 
 #initial position of players
 x_red=50
-y_red=200
+y_red=450
 x_blue=600
 y_blue=450
 x_blue_last=600
@@ -38,9 +38,16 @@ flag=0 #for normal game run
 angle=90
 lr=0
 ud=-1
+side=1
+
+
+tracker_x=[0,0,0,0,0]
+
+#to repeat
+pg.key.set_repeat(10, 5)
 
 #Direction map
-#       +1
+#       +1 
 #       / \ 
 # -1 <---0---> +1
 #       \ / 
@@ -48,9 +55,9 @@ ud=-1
 
 i=True
 while i:
-    
+
     if flag==0: #normal game
-        x,y=fn.get_ballpos(angle,lr,ud)
+        x,y,angle,lr,ud=fn.get_ballpos(x,y,angle,lr,ud)
 
     for event in pg.event.get():
         if event.type==pg.QUIT:
@@ -58,12 +65,22 @@ while i:
 
         if event.type==pg.KEYDOWN:
             if event.key==pg.K_RIGHT:
-                x_blue,lr=fn.get_playerbpos(x,y,x_blue,y_blue,lr)
+                x_blue=fn.get_playerbpos(x,y,x_blue,y_blue,lr,side,tracker_x,'r')
             if event.key==pg.K_LEFT:
-                x_blue,lr=fn.get_playerbpos(x,y,x_blue,y_blue,lr)
-        
-            
+                x_blue=fn.get_playerbpos(x,y,x_blue,y_blue,lr,side,tracker_x,'l')
 
+    screen.blit(bg, (0,0))
+    screen.blit(rod, (393,300))
+    screen.blit(playerr, (x_red,y_red))
+    screen.blit(playerb, (x_blue,y_blue))
+    screen.blit(volleyball, (x,y))
+
+    tracker_x=fn.tracker_ballx(tracker_x,x)
+    side=fn.ballside(x,tracker_x,lr,side)
+
+    pg.display.update()
+
+pg.quit()
 
 
 
