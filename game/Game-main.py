890205abf63b5b17007a90dde152754ea720db80
Ahish <ahishjayant9009@gmail.com>
2017-10-24 23:07:43 +0400

    Update Game-main.py

--- game/Game-main.py
+++ game/Game-main.py
@@ -10,6 +10,11 @@ screen_size=(800,600)
 #loads and transforms images
 bg,rod,playerr,playerb,volleyball=fn.load_images()
 
+#loads fonts
+TNR1=pg.font.SysFont("Times New Roman", 50)
+TNR2=pg.font.SysFont("Times New Roman", 30)
+TNR3=pg.font.SysFont("Times New Roman", 50)
+
 #initializes object for framerate
 clock=pg.time.Clock()
 
@@ -44,6 +49,14 @@ tracker_Rx=[0,0,0,0,0]
 tracker_x=[0,0,0,0,0]
 tracker_y=[0,0,0,0,0]
 
+red_score=0
+blue_score=0
+red_touch=0
+blue_touch=0
+
+delay_b=False
+delay_r=False
+
 jumpblue=False
 
 #to repeat
@@ -58,8 +71,37 @@ pg.key.set_repeat(10, 5)
 
 i=True
 while i:
+    scorer=TNR1.render(str(red_score)+' - '+str(blue_score),1,(0,0,0))
+    bluetouch=TNR2.render(str(3-blue_touch),1,(50,50,50))
+    
+    if red_score==15:
+        redwon=TNR3.render('Sorry, the computer has won...',1,(0,0,0))
+        flag=1
+    elif blue_score==15:
+        flag=2
+        bluewon=TNR3.render('Congrats! You beat the computer!!',1,(0,0,0))
+
+    if flag==1: #if red wins
+        screen.blit(bg, (0,0))
+        screen.blit(redwon, (50,250))
+        screen.blit(scorer,(300,330))
+
+    if flag==2: #if blue wins
+        screen.blit(bg, (0,0))
+        screen.blit(bluewon, (50,250))
+        screen.blit(scorer,(300,330))
     
     if flag==0: #normal game
+        
+        if delay_b: #delay to get contact as True after hitting the player once
+            if delay_bcount==50:
+                delay_b=False
+            delay_bcount+=1
+
+        if delay_r: #delay to get contact as True after hitting the player once
+            if delay_rcount==50:
+                delay_r=False
+            delay_rcount+=1
         
         if jumpblue:
             y_blue,jumpblue,jumpbcount=fn.bluejump(y_blue,jumpblue,jumpbcount)
@@ -67,7 +109,6 @@ while i:
         #ai
         x_red=fn.get_AI(side,lr,x,x_red)
         
-
         #get velocity of blue player
         bluevel=fn.get_bluevel(tracker_Bx)
 
@@ -81,10 +122,11 @@ while i:
         contact_sides=fn.checkcontact_sides(x)
 
         #checks if the ball touches the blue player
-        #to check if ball is in contact with the side of the player
-        contact_BLUEe,pos_Be=fn.checkcontact_BLUEedge(x,y,x_blue,y_blue)
-        #to check if the ball is in contact with the top of the player
-        contact_BLUEt=fn.checkcontact_BLUEtop(x,y,x_blue,y_blue,tracker_y)
+        if not delay_b:
+            #to check if ball is in contact with the side of the player
+            contact_BLUEe,pos_Be=fn.checkcontact_BLUEedge(x,y,x_blue,y_blue)
+            #to check if the ball is in contact with the top of the player
+            contact_BLUEt=fn.checkcontact_BLUEtop(x,y,x_blue,y_blue,tracker_y)
         #to check if the ball is in contact with the side of the player
         contact_BLUEs,pos_s=fn.checkcontact_BLUEside(x,y,x_blue,y_blue,tracker_x,lr)
 
@@ -97,12 +139,15 @@ while i:
         contact_RODt=fn.checkcontact_RODtop(x,y,tracker_y)
 
         #checks if the ball touches the red player
-        #to check if the ball is in contact with the edge of the red player
-        contact_REDe,pos_Re=fn.checkcontact_REDedge(x,y,x_red,y_red)
-        #to check if the ball is in contact with the top of the red player
-        contact_REDt=fn.checkcontact_REDtop(x,y,x_red,y_red,tracker_y)
+        if not delay_r:
+            #to check if the ball is in contact with the edge of the red player
+            contact_REDe,pos_Re=fn.checkcontact_REDedge(x,y,x_red,y_red)
+            #to check if the ball is in contact with the top of the red player
+            contact_REDt=fn.checkcontact_REDtop(x,y,x_red,y_red,tracker_y)
+        #to check if the ball is in contact with the side of the red player
+        contact_REDs,pos_Rs=fn.checkcontact_REDside(x,y,x_red,y_red,tracker_x,lr)
 
-        angle,lr,ud,y=fn.get_details_s1(y,angle,lr,ud,pos_Be,pos_Re,pos_s,toinvert,bluevel,redvel,contact_top,contact_sides,contact_RODs,contact_RODe,contact_RODt,contact_BLUEe,contact_BLUEt,contact_BLUEs,contact_REDe,contact_REDt)
+        angle,lr,ud,y=fn.get_details_s1(y,y_blue,angle,lr,ud,pos_Be,pos_Re,pos_s,pos_Rs,toinvert,bluevel,redvel,contact_top,contact_sides,contact_RODs,contact_RODe,contact_RODt,contact_BLUEe,contact_BLUEt,contact_BLUEs,contact_REDe,contact_REDt,contact_REDs)
 
         if angle<25:
             angle=25
@@ -111,6 +156,66 @@ while i:
             angle=155
         
         x,y,angle,lr,ud=fn.get_ballpos(x,y,angle,lr,ud)
+
+        if contact_BLUEe or contact_BLUEt:
+            delay_b=True
+            contact_BLUEe,contact_BLUEt=False,False
+            delay_bcount=0
+            blue_touch+=1
+            red_touch=0
+
+        if contact_REDe or contact_REDt:
+            delay_r=True
+            contact_REDe,contact_REDt=False,False
+            delay_rcount=0
+            red_touch+=1
+            blue_touch=0
+
+        if y+72>600:
+            pg.time.wait(1000)
+            if side==1:
+                red_score+=1
+                x=200
+            if side==-1:
+                blue_score+=1
+                x=600
+            y=100
+            lr,ud=0,-1
+            angle=90
+            red_touch,blue_touch=0,0
+            
+        if blue_touch==4:
+            pg.time.wait(1000)
+            red_score+=1
+            x=200
+            y=100
+            lr,ud=0,-1
+            angle=90
+            blue_touch,blue_touch=0,0
+        if red_touch==4:
+            pg.time.wait(1000)
+            blue_score+=1
+            x=600
+            y=100
+            lr,ud=0,-1
+            angle=90
+            red_touch,blue_touch=0,0
+            
+        screen.blit(bg, (0,0))
+        screen.blit(rod, (393,300))
+        screen.blit(playerr, (x_red,y_red))
+        screen.blit(playerb, (x_blue,y_blue))
+        screen.blit(scorer, (350,0))
+        screen.blit(bluetouch, (770,5))
+        screen.blit(volleyball, (x,y))
+
+        lr=fn.lr_checker(tracker_x,x) #as lr can go wrong
+
+        tracker_Bx=fn.tracker_BLUEx(tracker_Bx,x_blue)
+        tracker_Rx=fn.tracker_REDx(tracker_Rx,x_red)
+        tracker_x=fn.tracker_ballx(tracker_x,x)
+        tracker_y=fn.tracker_bally(tracker_y,y)
+        side=fn.ballside(x,tracker_x,lr,side)
 
     for event in pg.event.get():
         if event.type==pg.QUIT: 
@@ -126,24 +231,7 @@ while i:
                 if not jumpblue:
                     jumpblue=True
                     jumpbcount=0
-
-    if y+72>600:
-        y=100
-
-    screen.blit(bg, (0,0))
-    screen.blit(rod, (393,300))
-    screen.blit(playerr, (x_red,y_red))
-    screen.blit(playerb, (x_blue,y_blue))
-    screen.blit(volleyball, (x,y))
-
-    lr=fn.lr_checker(tracker_x,x) #as lr can go wrong
-
-    tracker_Bx=fn.tracker_BLUEx(tracker_Bx,x_blue)
-    tracker_Rx=fn.tracker_REDx(tracker_Rx,x_red)
-    tracker_x=fn.tracker_ballx(tracker_x,x)
-    tracker_y=fn.tracker_bally(tracker_y,y)
-    side=fn.ballside(x,tracker_x,lr,side)
-
+        
     pg.display.update()
     clock.tick(100) #decides maximum fps
 
